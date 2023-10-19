021310aeca9ea96c2e1a87d4e109e68a2aa62fde
Mohamed MRI <mohamedmri.21@uom.lk>
2023-10-19 14:16:14 +0530

    All the Tailwind , routes succesfully routed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='mb-20'>
+
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Welcome to the home page</h1>
+
+            <p className='mx-auto leading-relaxed text-baase mb-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi doloribus sed, maiores minima, neque eligendi quo voluptatum consectetur molestiae omnis, nulla expedita ut quasi velit quisquam magni. Voluptatem, nulla deserunt.</p>
+
+            <p className='mx-auto leading-relaxed text-baase mb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam facere incidunt nulla atque quia minus ut alias perspiciatis aspernatur nesciunt optio doloremque debitis deserunt ea pariatur harum ex ad aperiam fugit, repudiandae accusamus deleniti esse quis! Magni impedit optio alias fugit aspernatur adipisci harum ex quasi minus nesciunt! Minima, mollitia!</p>
+
+            <p className='mx-auto leading-relaxed text-baase mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatibus exercitationem earum voluptatem incidunt delectus ipsa non debitis doloribus nesciunt facilis hic, reiciendis laudantium fugiat nisi, eum odio id tenetur consectetur vel cumque autem. Non praesentium possimus veniam porro maiores ipsum itaque rerum excepturi tempore aliquam nulla, repellat exercitationem quisquam eveniet culpa odit magni nostrum autem accusantium. Omnis harum aspernatur illo reiciendis, odio necessitatibus facilis iure quasi ullam nesciunt eaque explicabo ab, pariatur, consectetur expedita. Commodi modi fugit, nobis et voluptate nulla perspiciatis itaque quasi officia quae voluptatem sint. Illum aspernatur neque aliquid accusamus voluptatem voluptas harum eligendi, quos laudantium.</p>
+            
+        </div>
+    )
+}
+
+export default Home
